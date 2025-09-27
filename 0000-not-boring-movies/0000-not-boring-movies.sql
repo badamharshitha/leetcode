a150135a79bead3eb274c9077cac55cda917afd2
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2025-09-27 21:31:51 +0530

    Time: 213 ms (93.34%), Space: 0B (100%) - LeetHub

--- 0000-not-boring-movies/0000-not-boring-movies.sql
+++ 0000-not-boring-movies/0000-not-boring-movies.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select id,movie,description,rating from cinema where id%2!=0 and description!='boring' order by rating desc;
+# Write your MySQL query statement below
+select * from cinema where (id%2=1) and description NOT LIKE 'boring' order by rating desc;
