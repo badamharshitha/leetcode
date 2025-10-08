d1f604c5f10e9d0eefc1d0298ec1af066426fe9a
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2025-10-08 14:30:21 +0530

    Time: 3 ms (74.09%), Space: 50.5 MB (27.4%) - LeetHub

--- 0050-majority-element-ii/0050-majority-element-ii.java
+++ 0050-majority-element-ii/0050-majority-element-ii.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         ArrayList<Integer> list=new ArrayList<>();
-        if(nums.length==1){
+        if(nums==null||nums.length==0)return list;
+        if(nums.length==1)
+        {
             list.add(nums[0]);
             return list;
         }
-        int cand1=nums[0];
-        int count1=1;
-        int cand2=0;
-        int count2=0;
-        for(int i=1;i<nums.length;i++){
-          if(cand1==nums[i])count1++;//1
-          if(cand2==nums[i])count2++;//0
-          if(count1==0){
-            count1=1;
-            cand1=nums[i];
-           }
-           if(cand1!=nums[i]&&count2==0){cand2=nums[i];
-           count2=1;}
-           if(cand1!=nums[i]&&cand2!=nums[i]){
-            count1--;//
-            count2--;
-           }
-
-        
+        int cand1=0,count1=0,cand2=0,count2=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(cand1==nums[i])count1++;
+            else if(cand2==nums[i])count2++;
+            else if(count1==0){
+                cand1=nums[i];
+                count1++;
+            }
+            else if(count2==0){
+                cand2=nums[i];
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-           if(count1>nums.length/3)list.add(cand1);
-           if(count2>nums.length/3)list.add(cand2);
-   
+        count1=0;
+        count2=0;
+         for(int i:nums){
+       if(i==cand1)count1++;
+        if(i==cand2) count2++;
+         }
+         if(count1>nums.length/3)list.add(cand1);
+         if(count2>nums.length/3&&cand1!=cand2)list.add(cand2);
         return list;
     }
 }
