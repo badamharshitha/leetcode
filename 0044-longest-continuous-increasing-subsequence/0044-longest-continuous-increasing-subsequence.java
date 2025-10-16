d6fbfb436d3bb16a92d911068f014c3d89cb5fcf
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2025-10-16 20:47:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0044-longest-continuous-increasing-subsequence/0044-longest-continuous-increasing-subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int count=1;
+        int maxcount=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]<nums[i+1])count++;
+            else count=1;
+            if(count>maxcount)maxcount=count;
+        }
+        return maxcount;
+    }
+}
