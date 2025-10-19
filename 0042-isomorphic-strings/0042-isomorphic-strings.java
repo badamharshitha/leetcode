d78c5fd26704118a80f7da027fccf058bb678222
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2025-10-19 20:28:18 +0530

    Time: 12 ms (56.68%), Space: 42.7 MB (23.91%) - LeetHub

--- 0042-isomorphic-strings/0042-isomorphic-strings.java
+++ 0042-isomorphic-strings/0042-isomorphic-strings.java
@@ -8,7 +8,7 @@ class Solution {
           if(t.charAt(i)!=map.get(s.charAt(i)))return false;}
           else {
             if(map.containsValue(t.charAt(i)))return false;
-          else map.put(s.charAt(i),t.charAt(i));}
+           map.put(s.charAt(i),t.charAt(i));}
         }
         return true;
     }
