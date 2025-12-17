52398b330b659dc5ac72886e10058f58148d97e8
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2025-12-17 10:43:38 +0530

    Time: 2 ms (98.91%), Space: 83.5 MB (97.12%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -1,13 +1,11 @@
 class Solution {
-    public long getDescentPeriods(int[] prices) {
-        long count=0;
+    static public long getDescentPeriods(int[] prices) {
+        long sum=0, des=0;
         int prev=-1;
-        int sum=0;
-        for(int x:prices)
-        {
-           count=(x+1==prev?count:0)+1;
-           sum+=count;
-           prev =x;
+        for (int x : prices){
+            des=(x+1==prev?des:0)+1;
+            sum+=des;
+            prev=x;
         }
         return sum;
     }
