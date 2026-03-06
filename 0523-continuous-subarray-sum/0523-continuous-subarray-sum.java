14a510039346e54de036eb87f51bd76f8ad39b35
Harshitha Badam <150006638+badamharshitha@users.noreply.github.com>
2026-03-06 14:37:51 +0530

    Time: 44 ms (5.76%), Space: 133.7 MB (98.43%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -7,11 +7,12 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             sum+=nums[i];
+             if(!map.containsKey(sum%k))map.put(sum%k,i);
             if(map.containsKey(sum%k)){
                 int val=map.get(sum%k);
                 if(i-map.get(sum%k)>=2)return true;
             }
-            if(!map.containsKey(sum%k))map.put(sum%k,i);
+           
                 
         }
         return false;
